aaf96b9bc1df59e9955ac045036f74f594fa439d
ocurran21 <orlajcurran@gmail.com>
2024-09-15 13:01:43 +0100

    added styling to page title components and added some file structure

---
+++ orla_curran_design/src/components/PageTitleComponent/PageTitleComponent.js
@@ -0,0 +1,10 @@
+import "./PageTitleComponent.css"
+
+const PageTitle = () => {
+    return (
+        <h1 className="title">ORLA CURRAN DESIGN</h1>
+    );
+};
+
+export default PageTitle;
+
---
+++ orla_curran_design/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitleComponent from "../components/PageTitleComponent/PageTitleComponent";
+import "./HomePage.css"
+
+const HomePage = () => {
+    return (
+        <header className="titleContainer">
+            <PageTitleComponent/>
+        </header>
+    );
+};
+
+export default HomePage;
+
