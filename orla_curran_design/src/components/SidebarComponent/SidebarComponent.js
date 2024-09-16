67d35ed36004b795a5d38063119e3bb56c837a30
ocurran21 <orlajcurran@gmail.com>
2024-09-16 14:34:29 +0100

    Added close sidebar button

--- orla_curran_design/src/components/SidebarComponent/SidebarComponent.js
+++ orla_curran_design/src/components/SidebarComponent/SidebarComponent.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./SidebarComponent.scss"
+import closeArrow from "../../resources/images/buttonImages/closeSidebar/arrowOrange.png"
+
+let showSidebar = true;
 
 const SidebarComponent = () => {
+    const [showSidebar, setShowSidebar] = useState(true);
+
+    const closeSidebar = () => {
+        setShowSidebar(false);
+    }
+
     return (
-        // <button onClick="openSidebar()">
-        //     ->
-        // </button>
-        <div className="sidebarContainer">
-            <ul>
-                <li>HOME</li>
-                <li className="navSpacing">GALLERY</li>
-                <li className="navSpacing">ABOUT</li>
-            </ul>
+        <div>
+            { showSidebar &&
+                <div className="sidebarContainer">
+                    <ul>
+                        <li>HOME</li>
+                        <li className="navSpacing">GALLERY</li>
+                        <li className="navSpacing">ABOUT</li>
+                    </ul>
+                    <button className="closeSidebarButton" onClick={closeSidebar}>
+                        <img src={closeArrow} alt="Close Sidebar"></img>
+                    </button>
+                </div>
+            }
         </div>
     );
-}
-
-const openSidebar = () => {
-
 }
 
 export default SidebarComponent;
