39c6194a2a8fc266f4eb35a00e0f754f1b768484
ocurran21 <orlajcurran@gmail.com>
2024-10-23 16:18:26 +0100

    Images now correct sizes for detail view and detail view opens when images clicked

--- orla_curran_design/src/components/DetailViewComponent/DetailViewComponent.js
+++ orla_curran_design/src/components/DetailViewComponent/DetailViewComponent.js
@@ -1,11 +1,31 @@
 import "./DetailViewComponent.scss";
-import plumDrawing from "../../resources/images/art/stillLife/plum.png"
+import {useRef, useEffect} from "react";
 
-const DetailViewComponent = () => {
+const DetailViewComponent = ({ image }) => {
+
+    const imgRef = useRef(null);
+
+    useEffect(() => {
+        if (imgRef.current) {
+        const { width, height } = imgRef.current;
+        if (width < 300 || height < 300) { // Example size threshold
+            imgRef.current.style.setProperty('--scale-factor', '1.5');
+        } else if ( width > 600 || height > 600 ) {
+            imgRef.current.style.setProperty('--scale-factor', '0.5');
+        } else {
+            imgRef.current.style.setProperty('--scale-factor', '1');
+        }
+        }
+    }, [image]);
 
     return (
         <div className="detailView">
-            <img src={plumDrawing} alt="Coloured Pencil Sketch of a Plum" className="fullSizeImage"/>
+            <img 
+                ref={imgRef}
+                src={image.src} 
+                alt={image.alt} 
+                className="detailImage"
+            />
         </div>
     );
 };
--- orla_curran_design/src/pages/GalleryPage/GalleryPage.js
+++ orla_curran_design/src/pages/GalleryPage/GalleryPage.js
@@ -1,91 +1,56 @@
 import "./GalleryPage.scss"
 import SidebarComponent from "../../components/SidebarComponent/SidebarComponent";
-import sections from "../../constants/images";
 import DetailViewComponent from "../../components/DetailViewComponent/DetailViewComponent";
-import {useState} from "react";
 import FooterComponent from "../../components/FooterComponent/FooterComponent";
+import sections from "../../constants/images";
+import {useState} from "react";
 
-const GalleryPage = (images) => {
-    const [showDetailView, setShowDetailView] = useState(false);
+const GalleryPage = () => {
+  const [showDetailView, setShowDetailView] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-    const openDetailView = () => {
-        setShowDetailView(true);
-    }
+  const openDetailView = (image) => {
+    setSelectedImage(image);
+    setShowDetailView(true);
+  };
 
-    const closeDetailView = () => {
-        setShowDetailView(false);
-    }
+  const closeDetailView = () => {
+    setShowDetailView(false);
+    setSelectedImage(null);
+  };
 
-    return (
-            <div className="galleryPage">
-                <div className="galleryContainer">
-                    <SidebarComponent/>
-                    <div className="displayGallery">
-                        {sections.map((section, index) => (
-                            <div key={index}>
-                                <p className={index === 0 ? "topHeader" : ""}>{section.header}</p>
-                                <section className="galleryGrid">
-                                    {section.images.map((image) => (
-                                        <img
-                                            key={image.id}
-                                            src={image.src}
-                                            alt={image.alt}
-                                            className={image.styles}
-                                        />
-                                    ))}
-                                </section>
-                            </div>
-                        ))}
-                        {/* <p className="topHeader">Still Life</p>
-                        <section className="galleryGrid">
-                            <img src={strawberryDrawing} alt="Coloured Pencil Sketch of a Strawberry"
-                                className="wide" onClick={() => openDetailView()}></img>
-                            <img src={limeDrawing} alt="Coloured Pencil Sketch of a Lime"></img>
-                            <img src={pepperDrawing} alt="Coloured Pencil Sketch of a Red Pepper"></img>
-                            <img src={pomegranateDrawing} alt="Coloured Pencil Sketch of a Pomegranate" className="tall wide"></img>
-                            <img src={dragonTreeDrawing} alt="Coloured Pencil Sketch of a potted Dragon Tree"
-                                className="tall"></img>
-                            <img src={plumDrawing} alt="Coloured Pencil Sketch of a Plum" className="tall"
-                                onClick={() => openDetailView()}/>
-                        </section>
-                        <p>Portraiture & Life Drawing</p>
-                        <section className="galleryGrid">
-                            <img src={owenProfile} alt="Orange pencil sketch of a man's side profile" className="tall wide"></img>
-                            <img src={blueNude} alt="Blue life drawing of a man" className="tall"></img>
-                            <img src={goldNude} alt="Gold life drawing of a man" className="wide"></img>
-                            <img src={greenNude} alt="Green life drawing of a woman" className="tall"></img>
-                            <img src={pencilSketchNude} alt="Pencil Sketch life drawing of a woman"></img>
-                        </section>
-                        <p>Animals</p>
-                        <section className="galleryGrid">
-                            <img src={blueSquirrel} alt="Blue pencil drawing of a squirrel" className="wide"></img>
-                            <img src={tanithProfile} alt="Pencil drawing of a Siamese cat" className="tall"></img>
-                            <img src={tanithPortrait} alt="Pencil drawing of a Siamese cat" className="wide"></img>
-                            <img src={bobPortrait} alt="Pencil drawing of Bob" className="wide tall"></img>
-                            <img src={jasperPortrait} alt="Pencil drawing of a white cat with ginger patches"></img>
-                        </section>
-                        <p>Paintings</p>
-                        <section className="galleryGrid">
-                            <img src={killywoolFoyleView} alt="Oil painting of the view over the Foyle river" className="wide"></img>
-                            <img src={churchPoint} alt="Oil painting of the view of Castleward Bay at sunset" className="wide tall"></img>
-                            <img src={caledonianMacBrayne} alt="Oil painting of the Ferry in Ullapool" className="wide"></img>
-                            <img src={honeyPortrait} alt="Oil painting of a dog on the beach" className="wide"></img>
-                            <img src={cavtatWatercolour} alt="Stylised watercolour of a Kayak in Croatia"></img>
-                            <img src={stemWatercolour} alt="Stylised watercolour of green stems"></img>
-                        </section> */}
-                        <FooterComponent/>
-                        {showDetailView ?
-                            <div>
-                                <DetailViewComponent/>
-                                <button onClick={closeDetailView}>Close</button>
-                            </div>
-                            : ""
-                        }
-                    </div>
-                </div>
+  return (
+    <div className="galleryPage">
+      <div className="galleryContainer">
+        <SidebarComponent />
+        <div className="displayGallery">
+          {sections.map((section, index) => (
+            <div key={index}>
+              <p className={index === 0 ? "topHeader" : ""}>{section.header}</p>
+              <section className="galleryGrid">
+                {section.images.map((image) => (
+                  <img
+                    key={image.id}
+                    src={image.src}
+                    alt={image.alt}
+                    className={image.styles}
+                    onClick={() => openDetailView(image)}
+                  />
+                ))}
+              </section>
             </div>
-    );
+          ))}
+          {showDetailView && selectedImage && (
+            <div>
+              <DetailViewComponent image={selectedImage} />
+              <button onClick={closeDetailView}>Close</button>
+            </div>
+          )}
+          <FooterComponent />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GalleryPage;
-
