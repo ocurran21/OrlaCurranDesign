03888fbd3cf1236f560ec29a9bb7daae8dda7b02
ocurran21 <orlajcurran@gmail.com>
2024-10-23 13:43:19 +0100

    Images are now stored in a separate file

--- orla_curran_design/src/pages/GalleryPage/GalleryPage.js
+++ orla_curran_design/src/pages/GalleryPage/GalleryPage.js
@@ -1,38 +1,11 @@
 import "./GalleryPage.scss"
 import SidebarComponent from "../../components/SidebarComponent/SidebarComponent";
-
-import strawberryDrawing from "../../resources/images/art/stillLife/strawyberry.png"
-import limeDrawing from "../../resources/images/art/stillLife/lime.png"
-import pepperDrawing from "../../resources/images/art/stillLife/pepper.png"
-import pomegranateDrawing from "../../resources/images/art/stillLife/pomegranate.png"
-import dragonTreeDrawing from "../../resources/images/art/stillLife/dragonTree.png"
-import plumDrawing from "../../resources/images/art/stillLife/plum.png"
-
-import blueNude from "../../resources/images/art/lifeDrawing/blueLifeDrawing.png"
-import goldNude from "../../resources/images/art/lifeDrawing/goldenLifeDrawing.png"
-import greenNude from "../../resources/images/art/lifeDrawing/greenLifeDrawing.png"
-import pencilSketchNude from "../../resources/images/art/lifeDrawing/lifeDrawingPencil.png"
-
-import owenProfile from "../../resources/images/art/portraiture/owenOrange.png"
-
-import blueSquirrel from "../../resources/images/art/animals/blueSquirrel.png"
-import tanithProfile from "../../resources/images/art/animals/tanithProfile.png"
-import tanithPortrait from "../../resources/images/art/animals/tanithPortrait.png"
-import bobPortrait from "../../resources/images/art/animals/bobPortrait.png"
-import jasperPortrait from "../../resources/images/art/animals/jasperPortrait.png"
-
-import stemWatercolour from "../../resources/images/art/watercolour/watercolourStems.png"
-import cavtatWatercolour from "../../resources/images/art/watercolour/cavtatWatercolour.png"
-import killywoolFoyleView from "../../resources/images/art/paintings/killywoolFoyleView.png"
-import churchPoint from "../../resources/images/art/paintings/churchPoint.png"
-import caledonianMacBrayne from "../../resources/images/art/paintings/caledonianMacBrayne.png"
-import honeyPortrait from "../../resources/images/art/paintings/honeyPortrait.png"
-
+import sections from "../../constants/images";
 import DetailViewComponent from "../../components/DetailViewComponent/DetailViewComponent";
 import {useState} from "react";
 import FooterComponent from "../../components/FooterComponent/FooterComponent";
 
-const GalleryPage = () => {
+const GalleryPage = (images) => {
     const [showDetailView, setShowDetailView] = useState(false);
 
     const openDetailView = () => {
@@ -48,7 +21,22 @@ const GalleryPage = () => {
                 <div className="galleryContainer">
                     <SidebarComponent/>
                     <div className="displayGallery">
-                        <p className="topHeader">Still Life</p>
+                        {sections.map((section, index) => (
+                            <div key={index}>
+                                <p className={index === 0 ? "topHeader" : ""}>{section.header}</p>
+                                <section className="galleryGrid">
+                                    {section.images.map((image) => (
+                                        <img
+                                            key={image.id}
+                                            src={image.src}
+                                            alt={image.alt}
+                                            className={image.styles}
+                                        />
+                                    ))}
+                                </section>
+                            </div>
+                        ))}
+                        {/* <p className="topHeader">Still Life</p>
                         <section className="galleryGrid">
                             <img src={strawberryDrawing} alt="Coloured Pencil Sketch of a Strawberry"
                                 className="wide" onClick={() => openDetailView()}></img>
@@ -57,16 +45,16 @@ const GalleryPage = () => {
                             <img src={pomegranateDrawing} alt="Coloured Pencil Sketch of a Pomegranate" className="tall wide"></img>
                             <img src={dragonTreeDrawing} alt="Coloured Pencil Sketch of a potted Dragon Tree"
                                 className="tall"></img>
-                            <img src={plumDrawing} alt="Coloured Pencil Sketch of a potted a Plum" className="tall"
+                            <img src={plumDrawing} alt="Coloured Pencil Sketch of a Plum" className="tall"
                                 onClick={() => openDetailView()}/>
                         </section>
                         <p>Portraiture & Life Drawing</p>
                         <section className="galleryGrid">
                             <img src={owenProfile} alt="Orange pencil sketch of a man's side profile" className="tall wide"></img>
                             <img src={blueNude} alt="Blue life drawing of a man" className="tall"></img>
-                            <img src={goldNude} alt="Gold life drawing of a woman" className="wide"></img>
+                            <img src={goldNude} alt="Gold life drawing of a man" className="wide"></img>
                             <img src={greenNude} alt="Green life drawing of a woman" className="tall"></img>
-                            <img src={pencilSketchNude} alt="Pencil Sketch life drwaing of a woman"></img>
+                            <img src={pencilSketchNude} alt="Pencil Sketch life drawing of a woman"></img>
                         </section>
                         <p>Animals</p>
                         <section className="galleryGrid">
@@ -74,23 +62,17 @@ const GalleryPage = () => {
                             <img src={tanithProfile} alt="Pencil drawing of a Siamese cat" className="tall"></img>
                             <img src={tanithPortrait} alt="Pencil drawing of a Siamese cat" className="wide"></img>
                             <img src={bobPortrait} alt="Pencil drawing of Bob" className="wide tall"></img>
-                            <img src={jasperPortrait} alt="Pencil drawing of a white cat with giiner patches"></img>
+                            <img src={jasperPortrait} alt="Pencil drawing of a white cat with ginger patches"></img>
                         </section>
-                        {/*<p>Watercolours</p>*/}
-                        {/*<section className="galleryGrid">*/}
-                        {/*    <img src={stemWatercolour} alt="Stylised watercolour of green stems" className="wide tall"></img>*/}
-                        {/*    <img src={cavtatWatercolour} alt="Stylised watercolour of a Kayak in Croatia" className="wide"></img>*/}
-                        {/*</section>*/}
-                        {/*<p>Oils</p>*/}
                         <p>Paintings</p>
                         <section className="galleryGrid">
-                            <img src={killywoolFoyleView} alt="Oil painting of the view of the Foyle" className="wide"></img>
+                            <img src={killywoolFoyleView} alt="Oil painting of the view over the Foyle river" className="wide"></img>
                             <img src={churchPoint} alt="Oil painting of the view of Castleward Bay at sunset" className="wide tall"></img>
                             <img src={caledonianMacBrayne} alt="Oil painting of the Ferry in Ullapool" className="wide"></img>
                             <img src={honeyPortrait} alt="Oil painting of a dog on the beach" className="wide"></img>
                             <img src={cavtatWatercolour} alt="Stylised watercolour of a Kayak in Croatia"></img>
                             <img src={stemWatercolour} alt="Stylised watercolour of green stems"></img>
-                        </section>
+                        </section> */}
                         <FooterComponent/>
                         {showDetailView ?
                             <div>
