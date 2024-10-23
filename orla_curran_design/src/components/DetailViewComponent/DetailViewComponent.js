17d40aee0a1b4a9a603f36c88b4341bf121b60c2
ocurran21 <orlajcurran@gmail.com>
2024-10-23 21:23:47 +0100

    Can now cycle through images in detailView, next button hudden on last image and prev hidden on first image

--- orla_curran_design/src/components/DetailViewComponent/DetailViewComponent.js
+++ orla_curran_design/src/components/DetailViewComponent/DetailViewComponent.js
@@ -1,14 +1,55 @@
 import "./DetailViewComponent.scss";
+import {useState} from "react";
+import sections from "../../constants/images";
+import previousButton from "../../resources/images/buttonImages/navigatePrevious.png"
+import nextButton from "../../resources/images/buttonImages/navigateNext.png"
 
 const DetailViewComponent = ({ image }) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    let currentImage;
+    let totalNumberOfImages = sections.reduce((acc, section) => acc + section.images.length, 0);
+
+    const getImageById = (id) => {
+        for (let section of sections) {
+            for (let img of section.images) {
+                if (img.id === id) {
+                    return img;
+                }
+            }
+        }
+    };
+
+    const handleNext = () => {
+        if (currentImageIndex < totalNumberOfImages - 1) {
+            setCurrentImageIndex(currentImageIndex + 1);
+        }
+    }
+
+    const handlePrev = () => {
+        if (currentImageIndex > 0) {
+            setCurrentImageIndex(currentImageIndex - 1);
+        }
+    }
+
+    currentImage = getImageById(currentImageIndex);
 
     return (
         <div className="detailView">
+            { currentImageIndex >= 1 && (
+                <div>
+                    <button className="previousButton" onClick={handlePrev}><img src={previousButton}/></button>
+                </div>
+            )}
             <img 
-                src={image.src} 
-                alt={image.alt} 
+                src={currentImage.src} 
+                alt={currentImage.alt} 
                 className="detailImage"
             />
+            { currentImageIndex < totalNumberOfImages-1 && (
+                <div>
+                    <button className="nextButton" onClick={handleNext}><img src={nextButton}/></button>
+                </div>
+            )}
         </div>
     );
 };
