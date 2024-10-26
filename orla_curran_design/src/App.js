cfe4206c14218975eef633a8315907d357f4bb48
ocurran21 <orlajcurran@gmail.com>
2024-10-26 16:21:08 +0100

    disable left click on images

--- orla_curran_design/src/App.js
+++ orla_curran_design/src/App.js
@@ -4,8 +4,23 @@ import { ROUTES } from './constants/routes';
 import HomePage from "./pages/HomePage/HomePage";
 import GalleryPage from "./pages/GalleryPage/GalleryPage"
 import AboutPage from "./pages/AboutPage/AboutPage"
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        const handleContextMenu = (event) => {
+          if (event.target.tagName === 'IMG') {
+            event.preventDefault();
+          }
+        };
+    
+        document.addEventListener('contextmenu', handleContextMenu);
+    
+        return () => {
+          document.removeEventListener('contextmenu', handleContextMenu);
+        };
+    }, []);
+
     return (
         <Routes>
             <Route path={ROUTES.home} element={<HomePage/>} />
