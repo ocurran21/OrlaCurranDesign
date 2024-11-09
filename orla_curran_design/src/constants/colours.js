352738490894bb1f1ddd3c292ff76e3d31c8cc55
ocurran21 <orlajcurran@gmail.com>
2024-11-09 11:45:32 +0000

    Update to add more accessible colours and changed logo size

--- orla_curran_design/src/constants/colours.js
+++ orla_curran_design/src/constants/colours.js
@@ -1,44 +1,44 @@
 export const COLOURS = {
     defaultStyle: {
         className: 'defaultStyle',
-        primary: '#6f777c',
-        secondary: '#2d353b',
-        tertiary: '#ffffff'
+        primary: '#6F777C',
+        secondary: '#2D353B',
+        tertiary: '#FFFFFF'
     },
     redStyle: {
         className: 'redStyle',
-        primary: '#e13232',
-        secondary: '#fba4b2',
-        tertiary: '#ffffff'
+        primary: '#911111',
+        secondary: '#F29EAC',
+        tertiary: '#9307A2'
     },
     tealStyle: {
         className: 'tealStyle',
-        primary: '#0d4647',
-        secondary: '#faf5d8',
-        tertiary: '#70d0bd'
+        primary: '#0D4647',
+        secondary: '#FAF5D8',
+        tertiary: '#08997C'
     },
     purpleStyle: {
         className: 'purpleStyle',
-        primary: '#956fc7',
-        secondary: '#fbcea4',
-        tertiary: '#592693'
+        primary: '#6A32B3',
+        secondary: '#FFD8B3',
+        tertiary: '#C42B9E'
     },
     blueStyle: {
         className: 'blueStyle',
-        primary: '#101e6d',
-        secondary: '#d590f5',
-        tertiary: '#ffffff'
+        primary: '#101E6D',
+        secondary: '#BD6AE3',
+        tertiary: '#FFFB01'
     },
     greenStyle: {
         className: 'greenStyle',
-        primary: '#4a8d3f',
-        secondary: '#a0f4e3',
-        tertiary: '#17a4f6'
+        primary: '#2F5E37',
+        secondary: '#B9FBEE',
+        tertiary: '#0068F1'
     },
     orangeStyle: {
         className: 'orangeStyle',
-        primary: '#e9873b',
-        secondary: '#faebc3',
-        tertiary: '#eec326'
+        primary: '#CB5800',
+        secondary: '#F3DB99',
+        tertiary: '#7E61FF'
     }
 }
